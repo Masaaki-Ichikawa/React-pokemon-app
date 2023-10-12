d0e90f2a161aad95b714cec469a9b65822df9ebd
machaaki <matyaakitabasko@gmail.com>
2023-10-12 18:01:26 +0900

    ポケモンのデータ表示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card/Card';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -42,7 +43,11 @@ function App() {
         </>
       ) : (
         <>
-          <h1>ポケモンデータを取得できました。</h1>
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         </>
       )}
     </div>
